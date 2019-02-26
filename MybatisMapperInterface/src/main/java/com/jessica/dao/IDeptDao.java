47990e754f52b0a9e54cd7717251d3e7b81a3650
JessicaWin <baihechenlu@126.com>
2019-02-26 09:41:27 +0800

    #{} and ${} in mybatis

--- MybatisMapperInterface/src/main/java/com/jessica/dao/IDeptDao.java
+++ MybatisMapperInterface/src/main/java/com/jessica/dao/IDeptDao.java
@@ -3,7 +3,10 @@ package com.jessica.dao;
 import com.jessica.entity.Dept;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface IDeptDao {
     void insertDept2(Dept dept) throws IOException;
+    List<Dept> selectDept2(Dept dept) throws IOException;
+    List<Dept> selectAll(String tableName) throws IOException;
 }
--- MybatisMapperInterface/src/test/java/com/jessica/dao/TestDept.java
+++ MybatisMapperInterface/src/test/java/com/jessica/dao/TestDept.java
@@ -5,24 +5,28 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 public class TestDept {
   private  Properties properties;
   private SqlSession session;
+  private Logger logger = Logger.getLogger(TestDept.class);
   @Before
   public void init() throws IOException {
     this.properties = new Properties();
     this.properties.load(Resources.getResourceAsStream("config.properties"));
     InputStream is = Resources.getResourceAsStream("MyBatisConfig.xml");
     SqlSessionFactory factory =
-            new SqlSessionFactoryBuilder().build(is, "development2", properties);
+            new SqlSessionFactoryBuilder().build(is, "development", properties);
     this.session = factory.openSession();
   }
 
@@ -36,6 +40,27 @@ public class TestDept {
     iDeptDao.insertDept2(dept);
     session.commit();
   }
+
+  @Test
+    public void testSelect2() throws IOException{
+        Dept dept = new Dept();
+        dept.setDepName("test");
+        dept.setLoc("shanghai");
+
+        IDeptDao iDeptDao = session.getMapper(IDeptDao.class);
+        List<Dept> res = iDeptDao.selectDept2(dept);
+        session.commit();
+        logger.debug(res);
+    }
+
+    @Test
+    public void testSelectAll() throws IOException{
+
+        IDeptDao iDeptDao = session.getMapper(IDeptDao.class);
+        List<Dept> res = iDeptDao.selectAll("dept2");
+        session.commit();
+        logger.debug(res);
+    }
 
   @After
   public void clear() {
